5e0ccbe1f540eb6870028620b2485a548ec84389
Clint Grimsley <cgrimsley@cccis.com>
2019-06-29 01:03:14 -0400

    made graphs prettier and created a markdown page for displaying them

---
+++ graphs.md
@@ -0,0 +1,17 @@
+## graphs
+
+### 3 hours
+
+![](graph_3h.png)
+
+### 6 hours
+![](graph_6h.png)
+
+### 24 hours
+![](graph_24h.png)
+
+### 7 days
+![](graph_7d.png)
+
+### 1 month
+![](graph_1M.png)
--- mon_battery.sh
+++ mon_battery.sh
@@ -18,13 +18,14 @@ function graph() {
     time=$1
     rrdtool \
     graph $projhome/graph_$time.png \
+    -c CANVAS#000000 -c FONT#FFFFFF -c BACK#000000 \
     --end now --start end-$time \
     --title "radmax battery level" \
-    -w 400 -h 300 \
+    -w 400 -h 200 \
     DEF:bat1_pct=$rrdfile:bat1_pct:AVERAGE:step=1 \
     DEF:bat0_pct=$rrdfile:bat0_pct:AVERAGE:step=1 \
     LINE1:bat1_pct#0000ff:"battery 1 level" \
-    LINE1:bat0_pct#005500:"battery 0 level" \
+    LINE1:bat0_pct#00ff00:"battery 0 level" \
     LINE1:0 &> /dev/null
 }
 
